38e30576766a2aa13b4f8a1c8dee11a873ea9a1c
mathieu.jee <mathieu.jee@heig-vd.ch>
2018-10-15 15:44:40 +0200

    Login, logout, registration

--- target.php
+++ target.php
@@ -1,3 +1,7 @@
+<?php
+    session_start();
+?>
+
 <!DOCTYPE html>
 
 <html>
@@ -6,16 +10,28 @@
        <meta charset="utf-8" />
    </head>
    <body>
-   <?php
-   if (isset($_POST['firstname']) AND isset($_POST['lastname']) AND isset($_POST['email'])) {
-    echo 'firstname: '     . strip_tags($_POST['firstname']) . '<br/>';
-    echo 'lastname: '      . strip_tags($_POST['lastname'])  . '<br/>';
-    echo 'email address: ' . strip_tags($_POST['email'])     . '<br/>';
-   }
-   else {
-        echo 'Fields not filled';
-   }
-   ?>
+<?php
+
+if (!empty($_POST)) {
+    if (isset($_POST['email'])) {
+        if($_POST['password'] == "toto") {
+            $_SESSION['email'] = strip_tags($_POST['email']);
+        }
+    }
+}
+
+if(isset($_SESSION['email']) AND $_POST['password'] == "toto") {
+    echo 'Welcome !</br>';
+    echo 'email address: ' . $_SESSION['email'] . '<br/>';
+}
+
+else {
+    header("Location: http://localhost/sti_projet_1/registration.php");
+}
+
+?>
    
+   <a href="logout.php">Logout</a>
+    
    </body>
 </html>
